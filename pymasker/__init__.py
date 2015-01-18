91f992b1988b36d1d7350161e41759b1ba294b6f
JoeYu <njkon@foxmail.com>
2015-01-18 01:17:27 -0500

    added basic setup script

---
+++ pymasker/__init__.py
@@ -0,0 +1,1 @@
+from .pymasker import masker
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymasker',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/dz316424/pymasker',
+      author='Haoliang Yu',
+      author_email='njkon@foxmail.com',
+      license='Apache',
+      packages=['pymasker'],
+      zip_safe=False)
