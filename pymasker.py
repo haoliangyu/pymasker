b5da0d39cfe993a4d6ff3f974b008da8f65a5d9d
haoliangyu <njkon@foxmail.com>
2016-08-07 19:16:36 -0400

    allow command line usage

--- pymasker.py
+++ pymasker.py
@@ -1,77 +1,78 @@
 import os
+import argparse
 import numpy as np
 
-class masker:
+class Masker(object):
     '''Provides access to functions that produces masks from remote sensing image, according to its bit structure.'''
 
     def __init__(self, band, *var):
 
         if type(band) is str:
             if len(var) > 0:
-                self.loadfile(band, var[0])
+                self.load_file(band, var[0])
             else:
-                self.loadfile(band)
+                self.load_file(band)
         else:
-            self.loadarray(band)
+            self.load_data(band)
 
-    def loadfile(self, filename, bandnum = 0):
+    def load_file(self, file_path, band_num = 0):
         '''Load the QA file from a give path
 
         Parameters
-            filename	-	Path of band file.
-            bandnum		-	Number of band.
+            file_path	-	Path of band file.
+            band_num	-	Number of band.
         '''
         import gdal
 
-        self.filename = filename
-        extension = os.path.splitext(filename)[1].lower()
+        self.file_path = file_path
+        extension = os.path.splitext(file_path)[1].lower()
 
         # load file according to the file format.
         if extension == '.hdf':
-            dataset = gdal.Open(filename)
-            subdataset = dataset.GetSubDatasets()[bandnum][0]
-            self.bandarray = gdal.Open(subdataset).ReadAsArray()
+            dataset = gdal.Open(file_path)
+            subdataset = dataset.GetSubDatasets()[band_num][0]
+            self.band_data = gdal.Open(subdataset).ReadAsArray()
         else:
-            bandfile = gdal.Open(filename)
-            self.bandarray = bandfile.GetRasterBand(1).ReadAsArray()
+            bandfile = gdal.Open(file_path)
+            self.band_data = bandfile.GetRasterBand(1).ReadAsArray()
 
-    def loadarray(self, array):
+    def load_data(self, array):
         '''Load the BQA ban from a np.array
 
         Parameters
             array		-	Numpy array that contains the band data.
         '''
-        self.filename = None
-        self.bandarray = array
+        self.file_path = None
+        self.band_data = array
 
-    def getmask(self, bitpos, bitlen, value):
+    def get_mask(self, bit_pos, bit_len, value):
         '''Generates mask with given bit information.
 
         Parameters
-            bitpos		-	Position of the specific QA bits in the value string.
-            bitlen		-	Length of the specific QA bits.
+            bit_pos		-	Position of the specific QA bits in the value string.
+            bit_len		-	Length of the specific QA bits.
             value  		-	A value indicating the desired condition.
         '''
-        lenstr = ''
-        for i in range(bitlen):
-            lenstr += '1'
-        bitlen = int(lenstr, 2)
+        len_str = ''
+        for i in range(bit_len):
+            len_str += '1'
+        bitlen = int(len_str, 2)
 
         if type(value) == str:
             value = int(value, 2)
 
-        posValue = bitlen << bitpos
-        conValue = value << bitpos
-        mask = (self.bandarray & posValue) == conValue
+        pos_value = bitlen << bit_pos
+        con_value = value << bit_pos
+        mask = (self.band_data & pos_value) == con_value
 
         return mask.astype(int)
 
-    def savetif(self, mask, filepath):
+    def save_tif(self, mask, file_path):
         '''Save the given mask as a .tif file.
 
         Parameters
             mask 		-	A mask generated with masker.
-            filepath	-	Path of .tif file.
+            file_path	-	Path of .tif file.
         '''
         import gdal
 
@@ -80,16 +81,16 @@ class masker:
         x_pixels = mask.shape[1]
         y_pixels = mask.shape[0]
 
-        dataset = driver.Create(filepath, x_pixels, y_pixels, 1, gdal.GDT_Int32)
+        dataset = driver.Create(file_path, x_pixels, y_pixels, 1, gdal.GDT_Int32)
 
-        if self.filename is not None:
-            extension = os.path.splitext(self.filename)[1].lower()
+        if self.file_path is not None:
+            extension = os.path.splitext(self.file_path)[1].lower()
             if extension == '.hdf':
-                hdfdataset = gdal.Open(self.filename)
+                hdfdataset = gdal.Open(self.file_path)
                 subdataset = hdfdataset.GetSubDatasets()[0][0]
                 bandfile = gdal.Open(subdataset)
             else:
-                bandfile = gdal.Open(self.filename)
+                bandfile = gdal.Open(self.file_path)
 
             dataset.SetGeoTransform(bandfile.GetGeoTransform())
             dataset.SetProjection(bandfile.GetProjectionRef())
@@ -97,12 +98,12 @@ class masker:
         dataset.GetRasterBand(1).WriteArray(mask)
         dataset.FlushCache()
 
-class confidence():
-    '''Levels of confidence that a condition exists
+class LandsatConfidence(object):
+    '''Level of confidence that a condition exists
 
-     high 		-	Algorithm has high confidence that this condition exists (67-100 percent confidence).
-     medium 		-	Algorithm has medium confidence that this condition exists (34-66 percent confidence).
-     low 		-	Algorithm has low to no confidence that this condition exists (0-33 percent confidence)
+    high 		-	Algorithm has high confidence that this condition exists (67-100 percent confidence).
+    medium 		-	Algorithm has medium confidence that this condition exists (34-66 percent confidence).
+    low 		-	Algorithm has low to no confidence that this condition exists (0-33 percent confidence)
     undefined	- 	Algorithm did not determine the status of this condition.
     none		-	Nothing.
     '''
@@ -112,29 +113,38 @@ class confidence():
     undefined = 0
     none = -1
 
-class landsatmasker(masker):
+class LandsatMasker(Masker):
     '''Provides access to functions that produces masks from quality assessment band of Landsat 8.'''
 
-    def getcloudmask(self, conf, cirrus = True, cumulative = False):
+    def get_cloud_mask(self, conf, cumulative = False):
         '''Generate a cloud mask.
 
         Parameters
             conf		-	Level of confidence that cloud exists.
-            cirrus		-	A value indicating whether the mask includes cirrus.
             cumulative	-	A Boolean value indicating whether the masking is cumulative.
 
         Return
             mask 		-	A two-dimension binary mask.
         '''
 
-        mask = self.__masking(14, 3, conf, cumulative)
-
-        if cirrus:
-            mask = self.__masking2(mask, 12, 3, conf, False, cumulative)
-
+        mask = self.__get_mask(14, 3, conf, cumulative)
         return mask.astype(int)
 
-    def getvegmask(self, conf, cumulative = False):
+    def get_cirrus_mask(self, conf, cumulative = False):
+        '''Generate a cirrus mask.
+
+        Parameters
+            conf		-	Level of confidence that cloud exists.
+            cumulative	-	A Boolean value indicating whether the masking is cumulative.
+
+        Return
+            mask 		-	A two-dimension binary mask.
+        '''
+
+        mask = self.__get_mask(12, 3, conf, cumulative)
+        return mask.astype(int)
+
+    def get_veg_mask(self, conf, cumulative = False):
         '''Generate a vegetation mask.
 
         Parameters
@@ -144,9 +154,9 @@ class landsatmasker(masker):
         Return
             mask 		-	A two-dimension binary mask.
         '''
-        return self.__masking(8, 3, conf, cumulative).astype(int)
+        return self.__get_mask(8, 3, conf, cumulative).astype(int)
 
-    def getwatermask(self, conf, cumulative = False):
+    def get_water_mask(self, conf, cumulative = False):
         '''Generate a water body mask.
 
         Parameters
@@ -156,9 +166,9 @@ class landsatmasker(masker):
         Return
             mask 		-	A two-dimension binary mask.
         '''
-        return self.__masking(4, 3, conf, cumulative).astype(int)
+        return self.__get_mask(4, 3, conf, cumulative).astype(int)
 
-    def getsnowmask(self, conf, cumulative = False):
+    def get_snow_mask(self, conf, cumulative = False):
         '''Generate a water body mask.
 
         Parameters
@@ -168,15 +178,15 @@ class landsatmasker(masker):
         Return
             mask 		-	A two-dimension binary mask.
         '''
-        return self.__masking(10, 3, conf, cumulative).astype(int)
+        return self.__get_mask(10, 3, conf, cumulative).astype(int)
 
-    def getmultimask(self,
-                    cloud = confidence.none, cloud_cum = False,
-                    cirrus = confidence.none, cirrus_cum = False,
-                    snow = confidence.none, snow_cum = False,
-                    veg = confidence.none, veg_cum = False,
-                    water = confidence.none, water_cum = False,
-                    inclusive = False):
+    def get_multi_mask(self,
+        cloud = LandsatConfidence.none, cloud_cum = False,
+        cirrus = LandsatConfidence.none, cirrus_cum = False,
+        snow = LandsatConfidence.none, snow_cum = False,
+        veg = LandsatConfidence.none, veg_cum = False,
+        water = LandsatConfidence.none, water_cum = False,
+        inclusive = False):
         '''Get mask with multiple conditions.
 
         Parameters
@@ -198,70 +208,49 @@ class landsatmasker(masker):
 
         # Basic mask
         if inclusive:
-            mask = self.bandarray < 0
+            final_mask = self.band_data < 0
         else:
-            mask = self.bandarray >= 0
+            final_mask = self.band_data >= 0
 
-        # veg pixel
-        mask = self.__masking2(mask, 8, 3, veg, veg_cum, inclusive)
+        tasks = [
+            [8, veg, veg_cum],     # veg pixel
+            [10, snow, snow_cum],    # Snow pixel
+            [12, cirrus, cirrus_cum],    # Cirrus pixel
+            [14, cloud, cloud_cum],    # Cloud pixel
+            [4, water, water_cum]      # Water body pixel
+        ]
 
-        # Snow pixel
-        mask = self.__masking2(mask, 10, 3, snow, snow_cum, inclusive)
+        for task in tasks:
+            mask = self.__get_mask(task[0], 3, task[1], task[2])
 
-        # Cirrus pixel
-        mask = self.__masking2(mask, 12, 3, cirrus, cirrus_cum, inclusive)
+            if inclusive:
+                final_mask = np.logical_or(final_mask, mask)
+            else:
+                final_mask = np.logical_and(final_mask, mask)
 
-        # Cloud pixel
-        mask = self.__masking2(mask, 14, 3, cloud, cloud_cum, inclusive)
+        return final_mask.astype(int)
 
-        # Water body pixel
-        mask = self.__masking2(mask, 4, 3, water, water_cum, inclusive)
-
-        return mask.astype(int)
-
-    def __masking(self, bitloc, bitlen, value, cumulative):
+    def __get_mask(self, bit_loc, bit_len, value, cumulative):
         '''Get mask with specific parameters.
 
         Parameters
-            bitloc		-	Location of the specific QA bits in the value string.
-            bitlen		-	Length of the specific QA bits.
+            bit_loc		-	Location of the specific QA bits in the value string.
+            bit_len		-	Length of the specific QA bits.
             value  		-	A value indicating the desired condition.
             cumulative	-	A Boolean value indicating whether the masking is cumulative.
         '''
 
-        posValue = bitlen << bitloc
-        conValue = value << bitloc
+        pos_value = bit_len << bit_loc
+        con_value = value << bit_loc
 
         if cumulative:
-            mask = (self.bandarray & posValue) >= conValue
+            mask = (self.band_data & pos_value) >= con_value
         else:
-            mask = (self.bandarray & posValue) == conValue
+            mask = (self.band_data & pos_value) == con_value
 
         return mask
 
-    def __masking2(self, basemask, bitloc, bitlen, value, cumulative, inclusive):
-        ''''Get mask with specific parameters.
-
-        Parameters
-            basemask	-	Base mask.
-            bitloc		-	Location of the specific QA bits in the value string.
-            bitlen		-	Length of the specific QA bits.
-            value  		-	A value indicating the desired condition.
-            inclusive	-	A Boolean value indicating whether the masking is inclusive or exclusive.
-            cumulative	-	A Boolean value indicating whether the masking is cumulative.
-        '''
-
-        if value == confidence.none:
-            return basemask
-
-        mask = self.__masking(bitloc, bitlen, value, cumulative)
-
-        if inclusive:
-            return np.logical_or(basemask, mask)
-        else:
-            return np.logical_and(basemask, mask)
-
-class modisquality():
+class ModisQuality(object):
     '''Level of data quality of MODIS land products at each pixel.
 
     high		-	Corrected product produced at ideal quality for all bands.
@@ -275,10 +264,10 @@ class modisquality():
     low = 2,
     low_cloud = 3
 
-class modisqamasker(masker):
+class ModisMasker(masker):
     '''Provides access to functions that produce QA masks from quality assessment band of MODIS land products.'''
 
-    def getqamask(self, quality):
+    def get_qa_mask(self, quality):
         '''Get a quality mask.
 
         Parameters
@@ -288,12 +277,59 @@ class modisqamasker(masker):
             mask 		-	A two-dimension binary mask.
         '''
 
-        return self.getmask(0, 2, quality).astype(int)
+        return self.get_mask(0, 2, quality).astype(int)
 
-class modisproduct():
-    '''Class that indicates supported modis products.
-    '''
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--source', help='source type: landsat, modis', type=str)
+parser.add_argument('-i', '--input', help='input image file path', type=str)
+parser.add_argument('-o', '--output', help='output raster path')
 
+# landsat arguments
+parser.add_argument('-c', '--confidence', help='level of confidence that a condition exists in a landsat image: high, medium, low, undefined, none', action='store')
+parser.add_argument('-t', '--target', help='target object: cloud, cirrus, water, vegetation, snow', action='store')
 
-class modismasker(modisqamasker):
-    '''Provides access to functions to that produce various masks from quality assessment band of MODIS land products.'''
+# modis argument
+parser.add_argument('-b', '--band', help='Band number of QA band', action='store')
+parser.add_argument('-q', '--quality', help='Level of data quality of MODIS land products at each pixel: high, medium, low, low_cloud', action='store')
+
+args = parser.parse_args()
+
+if args.source == 'landsat':
+    conf_value = {
+        'high': LandsatConfidence.high,
+        'medium': LandsatConfidence.medium,
+        'low': LandsatConfidence.low,
+        'undefined': LandsatConfidence.undefined,
+        'none': LandsatConfidence.none
+    }
+
+    masker = LandsatMasker(args.input)
+
+    if args.taget == 'cloud':
+        mask = masker.get_cloud_mask(conf_value[args.confidence])
+    elif args.target == 'cirrus':
+        mask = masker.get_cirrus_mask(conf_value[args.confidence])
+    elif args.target == 'water':
+        mask = masker.get_water_mask(conf_value[args.confidence])
+    elif args.target == 'vegetation':
+        mask = masker.get_veg_mask(conf_value[args.confidence])
+    elif args.target == 'snow':
+        mask = masker.get_snow_mask(conf_value[args.confidence])
+    else:
+        raise Exception('Masker type %s is unrecongized.' % args.target)
+
+    masker.save_tif(mask, args.output)
+
+elif args.source == 'modis':
+    quality_value = {
+        'high': ModisQuality.high,
+        'medium': ModisQuality.medium,
+        'low': ModisQuality.low,
+        'low_cloud': ModisQuality.low_cloud
+    }
+
+    masker = ModisMasker(args.inout, args.band)
+    mask = masker.get_qa_mask(quality_value[args.quality])
+    masker.save_tif(args.output)
+else:
+    raise Exception('Given source %s is unrecongized.' % args.source)
